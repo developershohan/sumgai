f72817662e33032014a41821edf4c6e019bbe224
Shohan <dev.shohanur@gmail.com>
2024-08-27 12:16:52 +0600

    first draft

--- assests/js/main.js
+++ assests/js/main.js
@@ -50,3 +50,10 @@ $(document).ready(function() {
         protect: true
     });
 });
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.elements.name.value = '';
+  e.target.elements.email.value = '';
+  e.target.elements.message.value = '';
+});
